e11e72abfe090f598d18cb993251a66925ff487b
Zach Christensen <23529374+ZachChristensen28@users.noreply.github.com>
2022-07-09 11:38:59 -0600

    updating variables and adding README
    Signed-off-by: Zach Christensen <23529374+ZachChristensen28@users.noreply.github.com>

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# splunk-app-dev
+# App Dev
+
 For building and publishing Splunk Apps and Add-ons
+
+## Validate & Package
+
+1. slim validate <package>
+1. slim package -o <output_dir> <package>
+
+    ```shell
+    #i.e.
+    slim package -o ~/build/pacakage TA-linux_iptables
+    ```
+
+## Submit to Splunk
+
+1. validate -v
+1. validate submit <package>
+1. validate status
+1. validate report
--- validate.sh
+++ validate.sh
@@ -2,13 +2,23 @@
 #
 # Validate Splunk apps
 #
+# =======================================
+# Customize
+#
 script_path=$(realpath $0)
 BASE_DIR=$(dirname $script_path)
 TOKEN_FILE="${BASE_DIR}/token.txt"
 REQUEST_FILE="${BASE_DIR}/request.txt"
-REPORT_DIR="${HOME}/github/reports"
+REPORT_DIR="${HOME}/build/reports"
+API_LOGIN_URL='https://api.splunk.com/2.0/rest/login/splunk'
+API_VAL_URL='https://appinspect.splunk.com/v1/app/validate'
+API_REPORT_URL='https://appinspect.splunk.com/v1/app/report'
 temp=${2##*/}
 APP=${temp//./-}
+#
+# End Customization
+# =======================================
+
 
 # Create report directory
 if [[ ! -d $REPORT_DIR ]]; then
@@ -20,35 +30,35 @@ check_path() {
 }
 
 validate() {
-        if [[ -z $SPLUNK_PASS ]]; then
-                curl -X GET -u "${API_USER}" --url "https://api.splunk.com/2.0/rest/login/splunk" | jq -r .data.token > $TOKEN_FILE
+        if [[ -z $API_PASS ]]; then
+                curl -X GET -u "${API_USER}" --url "$API_LOGIN_URL" | jq -r .data.token > $TOKEN_FILE
         else
-                curl -X GET -u "${API_USER}:$SPLUNK_PASS" --url "https://api.splunk.com/2.0/rest/login/splunk" | jq -r .data.token > $TOKEN_FILE
+                curl -X GET -u "${API_USER}:${API_PASS}" --url "$API_LOGIN_URL" | jq -r .data.token > $TOKEN_FILE
         fi
 }
 
 submit() {
         check_path $1
         token=$(<$TOKEN_FILE)
-        curl -X POST -H "Authorization: bearer $token" -H "Cache-Control: no-cache" -F "app_package=@\"$1\"" --url "https://appinspect.splunk.com/v1/app/validate" | jq -r .links[1].href | awk -F / '{ print $5 }' > $REQUEST_FILE
+        curl -X POST -H "Authorization: bearer $token" -H "Cache-Control: no-cache" -F "app_package=@\"$1\"" --url "${API_VAL_URL}" | jq -r .links[1].href | awk -F / '{ print $5 }' > $REQUEST_FILE
 }
 
 submit_cloud() {
         check_path $1
         token=$(<$TOKEN_FILE)
-        curl -X POST -H "Authorization: bearer $token" -H "Cache-Control: no-cache" -F "app_package=@\"$1\"" -F "included_tags=cloud" --url "https://appinspect.splunk.com/v1/app/validate" | jq -r .links[1].href | awk -F / '{ print $5 }' > $REQUEST_FILE
+        curl -X POST -H "Authorization: bearer $token" -H "Cache-Control: no-cache" -F "app_package=@\"$1\"" -F "included_tags=cloud" --url "${API_VAL_URL}" | jq -r .links[1].href | awk -F / '{ print $5 }' > $REQUEST_FILE
 }
 
 get_status() {
         token=$(<$TOKEN_FILE)
         request=$(<$REQUEST_FILE)
-        curl -X GET -H "Authorization: bearer $token" --url https://appinspect.splunk.com/v1/app/validate/status/$request
+        curl -X GET -H "Authorization: bearer $token" --url "${API_VAL_URL}/status/$request"
 }
 
 get_report() {
         token=$(<$TOKEN_FILE)
         request=$(<$REQUEST_FILE)
-        curl -X GET -H "Authorization: bearer $token" -H "Cache-Control: no-cache" -H "Content-Type: text/html" --url "https://appinspect.splunk.com/v1/app/report/$request" > ${REPORT_DIR}/${request}.html
+        curl -X GET -H "Authorization: bearer $token" -H "Cache-Control: no-cache" -H "Content-Type: text/html" --url "${API_REPORT_URL}/$request" > ${REPORT_DIR}/${request}.html
 }
 
 help() {
